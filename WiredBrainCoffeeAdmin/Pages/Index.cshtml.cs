aa8717e0c000a51f4a72f1fc75e8730b4a2e711f
Ozgur Sarikamis <ozgur.sarikamis@activequote.com>
2024-05-08 17:16:44 +0100

    Data binding

--- WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
@@ -1,27 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.IO;
-using System.Text;
-using System.Text.Json;
-using System;
-using System.IO;
-using System.Threading.Tasks;
 using WiredBrainCoffeeAdmin.Data.Models;
 
 namespace WiredBrainCoffeeAdmin.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+		public List<SurveyItem> SurveyResults { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+	public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
         public void OnGet()
         {
-
-        }
+            var rawJson = System.IO.File.ReadAllText("wwwroot/sampledata/survey.json");
+            SurveyResults = JsonSerializer.Deserialize<List<SurveyItem>>(rawJson);
+		}
     }
 }
