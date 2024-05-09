40fc0fab047a54a07ccee56e9eedfcf8bd930505
Ozgur Sarikamis <ozgur.sarikamis@activequote.com>
2024-05-09 17:35:10 +0100

    Form design

--- WiredBrainCoffeeAdmin/Data/Models/Product.cs
+++ WiredBrainCoffeeAdmin/Data/Models/Product.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WiredBrainCoffeeAdmin.Data
+namespace WiredBrainCoffeeAdmin.Data.Models
 {
     public class Product
     {
--- WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
@@ -1,10 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffeeAdmin.Data.Models;
 
 namespace WiredBrainCoffeeAdmin.Pages.Products
 {
     public class AddProductModel : PageModel
     {
+	    public Product NewProduct { get; set; }
+
         public void OnGet()
         {
         }
