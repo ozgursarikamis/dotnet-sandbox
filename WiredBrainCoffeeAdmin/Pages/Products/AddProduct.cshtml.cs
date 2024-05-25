d8f316c8bc35cc31b3ea34a7ae22fa5c6fd1f806
Ozgur Sarikamis <ozgur.sarikamis@activequote.com>
2024-05-25 19:17:40 +0100

    Uploading files

--- WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
@@ -8,12 +8,17 @@ namespace WiredBrainCoffeeAdmin.Pages.Products
     public class AddProductModel : PageModel
     {
 	    public WiredContext WiredContext;
+	    public IWebHostEnvironment WebHostEnvironment { get; set; }
 
-	    public AddProductModel(WiredContext wiredContext)
+		public AddProductModel(
+		    WiredContext wiredContext,
+		    IWebHostEnvironment webHostEnvironment
+			)
 	    {
 		    WiredContext = wiredContext;
-	    }
-
+			WebHostEnvironment = webHostEnvironment;
+		}
+		
 	    [BindProperty]
 	    public Product NewProduct { get; set; }
 
@@ -22,14 +27,22 @@ namespace WiredBrainCoffeeAdmin.Pages.Products
 
 		}
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPost()
         {
 	        if (!ModelState.IsValid) return Page();
 
-			WiredContext.Products.Add(NewProduct);
-			var changes = WiredContext.SaveChanges();
+	        if (NewProduct.Upload is null) return RedirectToPage("ViewAllProducts");
 
-            return RedirectToPage("ViewAllProducts");
-        }
+	        NewProduct.ImageFileName = NewProduct.Upload.FileName;
+	        var filePath = Path.Combine(WebHostEnvironment.ContentRootPath, "wwwroot/images/menu", NewProduct.Upload.FileName);
+	        await using var fileStream = new FileStream(filePath, FileMode.Create);
+	        await NewProduct.Upload.CopyToAsync(fileStream);
+
+	        NewProduct.Created = DateTime.Now;
+	        WiredContext.Products.Add(NewProduct);
+	        await WiredContext.SaveChangesAsync();
+
+	        return RedirectToPage("ViewAllProducts");
+		}
 	}
 }
