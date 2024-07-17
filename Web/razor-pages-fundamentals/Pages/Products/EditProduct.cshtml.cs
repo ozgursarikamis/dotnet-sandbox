ee0a489d959ed4f6dee8ba866e1eb252de8c8940
Ozgur Sarikamis <Ozgur.Sarikamis@ActiveQuote.com>
2024-07-17 15:27:26 +0100

    Edit functionality

--- Web/razor-pages-fundamentals/Pages/Products/EditProduct.cshtml.cs
+++ Web/razor-pages-fundamentals/Pages/Products/EditProduct.cshtml.cs
@@ -1,11 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffeeAdmin.Data;
+using WiredBrainCoffeeAdmin.Data.Models;
 
 namespace WiredBrainCoffeeAdmin.Pages.Products
 {
     public class EditProductModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+	    private IProductRepository productRepo;
+	    private IWebHostEnvironment webEnvironment;
+		public EditProductModel(IProductRepository productRepo, IWebHostEnvironment webEnvironment)
+		{
+			this.productRepo = productRepo;
+			this.webEnvironment = webEnvironment;
+		}
+
+		[BindProperty] public Product EditProduct { get; set; }
+
+		[FromRoute] // populate this ONLY from the URL
+		public int Id { get; set; }
+
+		public void OnGet()
+		{
+			EditProduct = this.productRepo.GetById(Id);
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			if(!ModelState.IsValid) return Page();
+			// if (EditProduct.Upload is null) return RedirectToPage("ViewAllProducts");
+
+			if (EditProduct.Upload is not null)
+			{
+				EditProduct.ImageFileName = EditProduct.Upload.FileName;
+
+				var file = Path.Combine(webEnvironment.ContentRootPath, "wwwroot/images/menu",
+					EditProduct.Upload.FileName);
+
+				await using var reader = new FileStream(file, FileMode.Create);
+				await EditProduct.Upload.CopyToAsync(reader);
+			}
+
+			EditProduct.Created = DateTime.Now;
+			EditProduct.Id = Id;
+			this.productRepo.Add(EditProduct);
+
+			return RedirectToPage("ViewAllProducts");
+		}
     }
 }
--- Web/razor-pages-fundamentals/Program.cs
+++ Web/razor-pages-fundamentals/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<WiredContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("WiredBrain") ?? string.Empty);
 });
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
