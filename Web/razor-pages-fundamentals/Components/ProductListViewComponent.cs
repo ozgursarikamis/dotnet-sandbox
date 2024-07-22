cd88b2ce461a7f847011be7701115a3f2d685631
Ozgur Sarikamis <Ozgur.Sarikamis@ActiveQuote.com>
2024-07-22 11:24:32 +0100

    ViewComponents for repetitive HTML view

---
+++ Web/razor-pages-fundamentals/Components/ProductListViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WiredBrainCoffeeAdmin.Data;
+
+namespace WiredBrainCoffeeAdmin.Components
+{
+	public class ProductListViewComponent : ViewComponent
+	{
+		public IProductRepository ProductRepository { get; }
+
+		public ProductListViewComponent(IProductRepository productRepository)
+		{
+			this.ProductRepository = productRepository;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var items = ProductRepository.GetAll();
+			return View(items);
+		}
+	}
+}
