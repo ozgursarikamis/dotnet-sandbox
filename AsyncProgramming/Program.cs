da93ff2bf79eafac1da32f4e6bf5fca34e353c7d
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-02-23 01:42:55 +0300

    [ASYNC] Task: Without a return value

--- AsyncProgramming/Program.cs
+++ AsyncProgramming/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello world");
+﻿Console.WriteLine("Starting...");
+await DoSomethingAsync();
+Console.WriteLine("Console is finished.");
+
+return;
+
+static async Task DoSomethingAsync()
+{
+    Console.WriteLine("Task started");
+    await Task.Delay(3000);
+    Console.WriteLine("Task finished");
+}
