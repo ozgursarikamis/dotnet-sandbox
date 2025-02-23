f49bc89d5c11981b9fe251213787ee5f5fddb01a
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-02-23 04:16:08 +0300

    [ASYNC] Task's Progress: File Download sample

--- AsyncProgramming/TaskProgress.cs
+++ AsyncProgramming/TaskProgress.cs
@@ -4,8 +4,52 @@ public static class TaskProgress
 {
     public static async Task Run()
     {
-        await UsingIProgress();
+        // await UsingIProgress();
+        await DownloadLargeFileWithProgress();
     }
+    
+    #region DownloadLargeFileWithProgress
+    private static async Task DownloadLargeFileWithProgress()
+    {
+        const string fileUrl = "https://link.testfile.org/PDF100MB";
+        const string destinationPath = "pdf-10-bin.pdf";
+        var progress = new Progress<int>(percentage =>
+        {
+            Console.WriteLine($"Download progress: {percentage}%");
+        });
+        
+        await DownloadFileAsync(fileUrl, destinationPath, progress);
+    }
+
+    private static async Task DownloadFileAsync(string fileUrl, string destinationPath, IProgress<int> progress)
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync(fileUrl, HttpCompletionOption.ResponseHeadersRead);
+        response.EnsureSuccessStatusCode();
+        
+        var totalBytes = response.Content.Headers.ContentLength ?? 0;
+        var downloadedBytes = 0L;
+
+        await using var fileStream = new FileStream(destinationPath, FileMode.Create, FileAccess.Write, FileShare.None);
+        await using var stream = await response.Content.ReadAsStreamAsync();
+        var buffer = new byte[8192]; // 8 KB buffer
+        int bytesRead;
+        while ((bytesRead = await stream.ReadAsync(buffer)) > 0)
+        {
+            await fileStream.WriteAsync(buffer.AsMemory(0, bytesRead));
+            downloadedBytes += bytesRead;
+
+            // Calculate percentage progress
+            var progressPercentage = (int)((downloadedBytes * 100) / totalBytes);
+                    
+            // Report progress
+            progress.Report(progressPercentage);
+        }
+    }
+
+    #endregion
+
+    #region Basics
 
     private static async Task UsingIProgress()
     {
@@ -28,4 +72,5 @@ public static class TaskProgress
             progress.Report(i);
         }
     }
+    #endregion
 }
