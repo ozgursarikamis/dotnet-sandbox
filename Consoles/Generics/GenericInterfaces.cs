f1747bf5db62d2007d1ed493da2e66fd3d9624b7
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-03-05 02:29:08 +0300

    [GENERICS] Generic Interface: Repo example

--- Consoles/Generics/GenericInterfaces.cs
+++ Consoles/Generics/GenericInterfaces.cs
@@ -4,7 +4,20 @@ public static class GenericInterfaces
 {
     public static void Run()
     {
-        GenericInterface();
+        // GenericInterface();
+        GenericInterface2();
+    }
+
+    private static void GenericInterface2()
+    {
+        IRepository<string> stringRepo = new Repository<string>();
+        stringRepo.Add("Hello");
+        Console.WriteLine(string.Join(", ", stringRepo.GetAll()));
+        
+        IRepository<int> intRepo = new Repository<int>();
+        intRepo.Add(1);
+        intRepo.Add(2);
+        Console.WriteLine(string.Join(", ", intRepo.GetAll()));
     }
 
     private static void GenericInterface()
@@ -28,4 +41,18 @@ class ConsoleLogger<T> : ILogger<T>
 interface ILogger<T>
 {
     void Log(T message);
+}
+
+interface IRepository<T>
+{
+    void Add(T entity);
+    IEnumerable<T> GetAll();
+}
+
+internal class Repository<T> : IRepository<T>
+{
+    private List<T> _items = [];
+    public void Add(T entity) => _items.Add(entity);
+
+    public IEnumerable<T> GetAll() => _items;
 }
