a16666094b109834e72334234274817e1f22c4be
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-03-05 10:22:32 +0300

    [GENERICS] Predicate<T>

--- Consoles/Generics/Predicates.cs
+++ Consoles/Generics/Predicates.cs
@@ -4,7 +4,26 @@ public static class Predicates
 {
     public static void Run()
     {
-        PredicateBasics();
+        // PredicateBasics();
+        PredicateDefinition();
+    }
+
+    private static void PredicateDefinition()
+    {
+        // Predicate<T>
+        Point?[] points =
+        {
+            new(100, 200),
+            new(150, 250),
+            new(200, 300),
+            new(300, 400),
+        };
+        
+        // Define a Predicate<T> delegate:
+        Predicate<Point> predicate = FindPoints;
+        Point? first = Array.Find(points, predicate!);
+
+        Console.WriteLine($"{first?.X}, {first?.Y}");
     }
 
     private static void PredicateBasics()
@@ -20,4 +39,15 @@ public static class Predicates
         static bool IsEven(int number) => number % 2 == 0;
         static bool IsOdd(int number) => number % 2 != 0;
     }
+
+    private static bool FindPoints(Point p)
+    {
+        return p.X * p.Y > 100_000;
+    }
+}
+
+internal class Point(int x, int y)
+{
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
 }
