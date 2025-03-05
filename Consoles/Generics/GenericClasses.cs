fe89d136138de5e0034ac2340743b850fa3aea4b
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-03-05 09:56:29 +0300

    [GENERICS] Generic classes: Using Base Classes and Subclasses in Generics

--- Consoles/Generics/GenericClasses.cs
+++ Consoles/Generics/GenericClasses.cs
@@ -4,7 +4,17 @@ public static class GenericClasses
 {
     public static void Run()
     {
-        UsingGenericClass();
+        // UsingGenericClass();
+        GenericNotifier();
+    }
+
+    private static void GenericNotifier()
+    {
+        var emailNotifier = new Notifier<EmailNotification>();
+        emailNotifier.Send(new EmailNotification { Message = "You've got mail", Email = "test@email.com" });
+        
+        var smsNotifier = new Notifier<SMSNotification>();
+        smsNotifier.Send(new SMSNotification { Message = "Your OTP is 1234", PhoneNumber = "12345" });
     }
 
     private static void UsingGenericClass()
@@ -33,4 +43,16 @@ internal class GenericRepository<T>
 internal class Product
 {
     public string Title { get; set; }
+}
+
+public class Notification { public string Message { get; set; } }
+public class EmailNotification : Notification { public string Email { get; set; } }
+public class SMSNotification : Notification { public string PhoneNumber { get; set; } }
+
+public class Notifier<T> where T : Notification
+{
+    public void Send(T notification)
+    {
+        Console.WriteLine($"Sending notification: {notification.Message}");
+    }
 }
